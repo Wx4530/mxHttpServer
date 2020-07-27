5f515fa57ec911c6ccf89f7fface7060e5951b35
Wx4530 <wx4530_seu@163.com>
2020-07-27 06:21:28 -0700

    buffer类 一个可变大小的缓冲区

---
+++ base/Buffer.cpp
@@ -0,0 +1,94 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: wx
+ * @Date: 2020-07-24 12:28:39
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-07-27 20:53:23
+ */ 
+#include "Buffer.h"
+
+using namespace std;
+using namespace xnet;
+using namespace xnet::net;
+
+Buffer::Buffer(size_t initialSize = kInitialSize)
+    :   m_readIndex(kPreHeaderLen),
+        m_writeIndex(kPreHeaderLen)
+{
+    assert(readableBytes() == 0);
+    assert(writableBytes() == initialSize);
+    assert(leftBlank() == kPreHeaderLen);
+    m_buf.reserve(kPreHeaderLen + initialSize);
+}
+
+// 找到开始读取的地址
+const char* Buffer::peek()
+{
+    return begin() + m_readIndex;
+}
+
+void Buffer::append(const char* data, size_t len)
+{
+    
+}
+
+void Buffer::append(const void* data, size_t len)
+{
+    append(static_cast<const char*>(data), len);
+}
+
+void Buffer::write(const void* data, size_t len)
+{
+    // 空间不够则填入
+    if(len > writableBytes())
+    {
+        len *= sizeof(data);
+        makeSpace(len);
+    }
+    // 添加数据
+    std::copy(data, data + len, begin() + m_writeIndex);
+}
+
+
+// 交换两个Buffer对象
+void Buffer::swap(Buffer&)
+{
+
+}
+
+// 从文件描述符fd读取
+int Buffer::readf(int fd)
+{
+
+}
+
+int Buffer::writef(int fd)
+{
+
+}
+
+// 从缓冲区读取
+int Buffer::read(size_t n)
+{
+
+}
+
+void Buffer::makeSpace(size_t addlen)
+{
+    if(leftBlank() + writableBytes() < addlen)
+    {
+        m_buf.resize(m_writeIndex + addlen);
+    }
+    else
+    {
+        move2left();
+    }
+}
+// 将buffer中的数据左对齐到初始位置
+void Buffer::move2left()
+{
+    std::copy(begin() + m_readIndex, begin() + m_writeIndex, begin() + kPreHeaderLen);
+    m_writeIndex = m_writeIndex - m_readIndex + kPreHeaderLen;
+    m_readIndex = kPreHeaderLen;
+}
---
+++ base/Buffer.h
@@ -0,0 +1,110 @@
+/*
+ * @Description: Buffer类, 接管数据的收发, 隔离sockfd的读取和写入操作
+ * @Version: 1.0
+ * @Autor: wx
+ * @Date: 2020-07-24 12:28:39
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-07-27 20:52:06
+ */ 
+#ifndef _XNET_BUFFER_H_
+#define _XNET_BUFFER_H_
+
+
+#include <string>
+#include <vector>
+#include <assert.h>
+
+#include "../base/copyable.h"
+
+namespace xnet
+{
+namespace net
+{
+// 目前buffer类缓存的数据存放在栈区
+/*
+class : Buffer
+用于提供一块内存来作为tcp nonblock 通信套接字的缓冲区
+发送和接收数据时， 均先经过缓冲区buffer
+*/
+class Buffer : public xnet::copyable
+{
+public:
+    // 默认缓冲区大小， 
+    static const size_t kPreHeaderLen = 8;
+    static const size_t kInitialSize = 1024;
+
+    Buffer(size_t initialSize = kInitialSize);
+
+    // 返回可读的字节数， m_writeIndex - m_readIndex;
+    size_t readableBytes()
+    {
+        return m_writeIndex - m_readIndex;
+    }
+    // 返回可写的字节数， m_writeIndex - m_readIndex;
+    size_t writableBytes()
+    {
+        return capacity() - m_writeIndex;
+    }
+    // 返回vector左侧可写入区间的长度
+    int leftBlank()
+    {
+        return m_readIndex - kPreHeaderLen;
+    }
+    // 返回读写位置的指针
+    char* readPos()
+    {
+        return begin() + m_readIndex;
+    }
+    char* writePos()
+    {
+        return begin() + m_writeIndex;
+    }
+
+    const char* peek();
+    void swap(Buffer&);
+    void write(const void*, size_t);
+    // 从文件描述符fd读取
+    int readf(int fd);
+    // 写入文件描述符
+    int writef(int fd);
+    // 从缓冲区读取
+    int read(size_t n);
+    // 向缓冲区追加数据
+    void append(const char* data, size_t len);
+    void append(const void* data, size_t len);
+
+    // 更新读写下标
+    void updateIdx(size_t len);
+private:
+    // 返回缓冲区头部字符
+    char* begin()
+    {
+        return &*m_buf.begin();
+    }
+    const char* begin() const
+    {
+        return &*m_buf.begin();
+    }
+    size_t capacity()
+    {
+        return m_buf.capacity();
+    }
+    // 写入空间不足时调用,传入需要的空间大小
+    void makeSpace(size_t addlen);
+    // 将buffer中的数据左对齐到初始位置
+    void move2left();
+
+private:
+    // 类数据成员
+    std::vector<char> m_buf;
+    // m_buf: 0 <= readIndex <= m_writeIndex < m_buf.capacity();
+    // 两个下标把缓冲区分成三块， leftBlank, readableBytes, writableBytes;
+    size_t m_readIndex;
+    size_t m_writeIndex;
+};
+
+} // namespace net
+
+} // namespace xnet
+
+#endif // !_XNET_BUFFER_H_
